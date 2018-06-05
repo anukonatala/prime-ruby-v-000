4c4606fffb0628a631d83b31283043f00ce7913e
Anusha Konatala <anu.konatala@gmail.com>
2018-06-05 01:17:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,18 @@
 # Add  code here!
 
 def prime?(num)
-  if num == 1 || num == 2
+  if num == 2
     return true
-  end
-  for i in 2..n/2
+  elsif num == 1
+    return false
+  else
+    for i in 2..n/2
     temp=num%i;
     if temp == 0
       return false
     end
   end
   return true
+  end
+  
 end
